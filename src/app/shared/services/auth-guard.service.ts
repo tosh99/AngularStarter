29199840588980ab97728d86e54094535a0297fe
Anutosh <anutosh.chaudhuri@gmail.com>
2019-08-07 15:35:56 +0530

    Updated Global State Updated Routing Updated Angular Version

---
+++ src/app/shared/services/auth-guard.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, Router} from '@angular/router';
+import {GlobalStateVariables} from './globalstate.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(private router: Router, private globalStateService: GlobalStateVariables,) {
+    }
+
+    canActivate(): boolean {
+        let gbstate = localStorage.getItem('globalState');
+        if (gbstate === null || gbstate === undefined) {
+            this.router.navigateByUrl('/');
+            return false;
+        } else {
+            if (typeof (gbstate) === 'string') {
+                gbstate = JSON.parse(gbstate);
+            }
+
+            if (!gbstate['user']) {
+                this.router.navigateByUrl('/');
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
