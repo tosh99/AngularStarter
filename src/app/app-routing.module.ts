440bf99b807e30798052801cd2e06a29e2a9c75a
Anutosh <anutosh.chaudhuri@gmail.com>
2019-09-21 23:32:02 +0530

    Upgraded to Angular 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./samplemodule/samplemodule.module').then(m => m.SamplemoduleModule)
+    }
+];
+
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
